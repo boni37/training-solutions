76b8d7fb2b4fa06cf3f1fa797870bcf49f0e0037
boni37 <73605283+boni37@users.noreply.github.com>
2020-11-06 17:59:34 +0100

    Statement

---
+++ src/main/java/statements/StatementMain.java
@@ -0,0 +1,21 @@
+package statements;
+
+public class StatementMain {
+    public static void main(String[] args) {
+        int x = 5 + 6;
+        System.out.println(x);
+        int y = 11 - x;
+        System.out.println(y);
+        int z = 8;
+        System.out.println(z);
+        boolean b = x > y;
+        System.out.println(b);
+        boolean c = (b == true)|(z>5);
+        System.out.println(c);
+        z++;
+        System.out.println(z);
+    }
+
+
+
+}
