5643d2853f3a5b00a6861f420d194accd5e5bbbb
boni37 <73605283+boni37@users.noreply.github.com>
2020-11-06 10:21:10 +0100

    Languages

---
+++ src/main/java/week02/Languages.java
@@ -0,0 +1,13 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Languages {
+    public static void main(String[] args) {
+        String[] names = {"Java", "Python", "JavaScript"};
+        List<String> listLanguages= Arrays.asList(names);
+        System.out.println(listLanguages);
+    }
+}
