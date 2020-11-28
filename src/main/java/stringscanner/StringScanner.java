6ef9bc93747b560c8ba318a6ee285ab216975d7f
boni37 <73605283+boni37@users.noreply.github.com>
2020-11-28 18:02:19 +0100

    StringScanner

---
+++ src/main/java/stringscanner/StringScanner.java
@@ -0,0 +1,44 @@
+package stringscanner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StringScanner {
+
+    public int readAndSumValues(String intString, String delimiter){
+        Scanner s = new Scanner(intString).useDelimiter(delimiter);
+        int sum =0;
+        if (delimiter == null){
+            throw new NullPointerException("Incorrect parameter string!");
+        }
+        if(s.hasNextInt()){
+            while(s.hasNext()){
+                sum += s.nextInt();
+        }return sum;
+        } else {
+            throw new IllegalArgumentException("Incorrect parameter string!");
+        }
+    }
+
+    /*public int readAndSumValues(String intString){
+        Scanner s = new Scanner(intString);
+        int sum = 0;
+        return sum;
+
+    }
+
+    public String filterLinesWithWordOccurrences(String text, String word){
+        Scanner s = new Scanner(text);
+        List<String> l = new ArrayList<>();
+        if (s.findInLine(word))
+
+
+    }*/
+
+    public boolean isEmpty(String string){
+       return string == null || string == "";
+
+    }
+
+}
