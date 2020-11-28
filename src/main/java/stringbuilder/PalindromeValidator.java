19f07953dc95675b573862c1b486aa801ccab589
boni37 <73605283+boni37@users.noreply.github.com>
2020-11-28 13:25:06 +0100

    Palindrome

---
+++ src/main/java/stringbuilder/PalindromeValidator.java
@@ -0,0 +1,14 @@
+package stringbuilder;
+
+public class PalindromeValidator {
+
+    public boolean isPalindrome(String word){
+        if (word == null ) {
+            throw new IllegalArgumentException("Text must not be null!");
+        }
+
+        word = word.trim();
+        StringBuilder sb = new StringBuilder(word);
+        return word.equalsIgnoreCase(sb.reverse().toString());
+    }
+}
