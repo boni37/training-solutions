67ab22a0a05261c7a3c018f0ef738a5546d60cb1
boni37 <73605283+boni37@users.noreply.github.com>
2020-12-07 16:34:49 +0100

    week07d01

---
+++ src/main/java/week07d01/MathAlgorithms.java
@@ -0,0 +1,24 @@
+package week07d01;
+
+public class MathAlgorithms {
+
+    int n;
+
+    public static boolean isPrime(int n){
+        int m=0;
+        m=n/2;
+        if(n==0||n==1){
+            throw new IllegalArgumentException("It is not prime");
+        }else{
+            for(int i=2;i<=m;i++){
+                if(n%i==0){
+                    return false;
+                }
+        }
+        } return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(12));
+    }
+}
