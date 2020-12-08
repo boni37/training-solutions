c1d1157ef3374ff83d3fed38a415c8f21765fa90
boni37 <73605283+boni37@users.noreply.github.com>
2020-12-08 17:57:01 +0100

    DigitSum

---
+++ src/main/java/week07d02/DigitSum.java
@@ -0,0 +1,14 @@
+package week07d02;
+
+public class DigitSum {
+
+    public static int sumOfDigits(int x){
+        String number = String.valueOf(x);
+        int sum = 0;
+        for(int i=0; i< number.length();i++){
+            sum += Character.getNumericValue(number.charAt(i));
+        }
+        return sum;
+        }
+
+}
---
+++ src/test/java/week07d02/DigitSumTest.java
@@ -0,0 +1,15 @@
+package week07d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DigitSumTest {
+
+    @Test()
+        public void sumDigitTest(){
+           assertEquals(21,DigitSum.sumOfDigits(123456));
+        }
+
+
+}
