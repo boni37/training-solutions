43a85642cb3d76f18a68e2dc3af453d37bb55128
boni37 <73605283+boni37@users.noreply.github.com>
2020-12-09 16:25:05 +0100

    ObjectInheritance

---
+++ src/main/java/introinheritencia/BigBoss.java
@@ -0,0 +1,23 @@
+package introinheritencia;
+
+public class BigBoss extends Boss{
+
+    private double bonus;
+
+    public BigBoss(String name, String address, double salary, int numberOfEmployees, double bonus) {
+        super(name, address, salary, numberOfEmployees);
+        this.bonus = bonus;
+    }
+
+
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    @Override
+    public double getSalary() {
+        return super.getSalary()+bonus;
+    }
+
+}
---
+++ src/main/java/introinheritencia/Person.java
@@ -0,0 +1,25 @@
+package introinheritencia;
+
+public class Person {
+
+    private String name;
+    private String address;
+
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void migrate(String newAddress){
+        address = newAddress;
+    }
+}
