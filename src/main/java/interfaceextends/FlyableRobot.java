173f8a4f4e1c5e1d9c4b236c6d8ace186adba6c9
boni37 <73605283+boni37@users.noreply.github.com>
2020-12-31 17:24:35 +0100

    interfaceextends

---
+++ src/main/java/interfaceextends/FlyableRobot.java
@@ -0,0 +1,12 @@
+package interfaceextends;
+
+import abstractclass.gamecharacter.Point;
+
+import java.util.List;
+
+public interface FlyableRobot extends MoveableRobot{
+
+    void liftTo(long altitude);
+
+
+}
