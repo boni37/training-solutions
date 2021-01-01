e033a5752681cea42dbdfcfdd6a43de3ae0d72d1
boni37 <73605283+boni37@users.noreply.github.com>
2021-01-01 17:06:27 +0100

    enumabstract

---
+++ src/main/java/enumabstract/OrderState.java
@@ -0,0 +1,38 @@
+package enumabstract;
+
+public enum OrderState implements Deletable
+
+{NEW{
+    @Override
+    public boolean canDelete() {
+        return true;
+    }
+},CONFIRMED{
+    @Override
+    public boolean canDelete() {
+        return true;
+    }
+},PREPARED{
+    @Override
+    public boolean canDelete() {
+        return true;
+    }
+},DELIVERED{
+    @Override
+    public boolean canDelete() {
+        return false;
+    }
+},RETURNED{
+    @Override
+    public boolean canDelete() {
+        return false;
+    }
+},DELETED{
+    @Override
+    public boolean canDelete() {
+        return false;
+    }
+};
+
+
+}
---
+++ src/test/java/enumabstract/OrderStateTest.java
@@ -0,0 +1,53 @@
+package enumabstract;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OrderStateTest {
+
+    @Test
+    public void testCanDeleteOnNew() {
+        OrderState state = OrderState.NEW;
+
+        assertTrue(state.canDelete());
+    }
+
+    @Test
+    public void testCanDeleteOnConfirmed() {
+        OrderState state = OrderState.CONFIRMED;
+
+        assertTrue(state.canDelete());
+    }
+
+    @Test
+    public void testCanDeleteOnPrepared() {
+        OrderState state = OrderState.PREPARED;
+
+        assertTrue(state.canDelete());
+    }
+
+    @Test
+    public void testCanDeleteOnDelivered() {
+        OrderState state = OrderState.DELIVERED;
+
+        assertFalse(state.canDelete());
+    }
+
+    @Test
+    public void testCanDeleteOnReturned() {
+        OrderState state = OrderState.RETURNED;
+
+        assertFalse(state.canDelete());
+    }
+
+    @Test
+    public void testCanDeleteOnDeleted() {
+        OrderState state = OrderState.DELETED;
+
+        assertFalse(state.canDelete());
+    }
+
+
+}
