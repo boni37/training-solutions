367c1fbfaaee7270ea73fefe5b87a463df61a96a
boni37 <73605283+boni37@users.noreply.github.com>
2021-01-05 20:03:10 +0100

    week10d02

--- src/main/java/week10d02/MaxTravel.java
+++ src/main/java/week10d02/MaxTravel.java
@@ -1,15 +1,5 @@
 package week10d02;
 
-//Van egy egész számok listája, amelyben egy szám azt jelzi, hogy az adott számú buszmegállóban egy ember
-// fel akar szállni. A 12,12,0,3,4,4 sorozat tehát azt jelenti, hogy a 12-es buszmegállóban 2-en, a 0-ásban
-// 1 ember, 3-asban egy ember, 4-esben 2 ember akar felszállni. A MaxTravel osztály getMaxIndex() metódusa adja vissza,
-// hogy hanyas megállóban szeretnének a legtöbben felszállni. Maximum 30 megálló lehet.
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class MaxTravel {
 
     private static final int numStops = 30;
