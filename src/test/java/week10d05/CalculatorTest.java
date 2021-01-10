1281d5538268e73e3efb9d2390fa64468c2b0019
boni37 <73605283+boni37@users.noreply.github.com>
2021-01-10 09:28:13 +0100

    week10d05

--- src/test/java/week10d05/CalculatorTest.java
+++ src/test/java/week10d05/CalculatorTest.java
@@ -9,7 +9,7 @@ class CalculatorTest {
     @Test
     public void calculatorTest(){
         Calculator calculator = new Calculator();
-        int[] arr = {1,3,6,8,55,66,125};
+        int[] arr = {1,3,23,8,55,14,125};
         calculator.findMinSum(arr);
     }
 
