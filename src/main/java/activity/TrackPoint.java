f67b176aaac71d7429dd21545590c06c72e1b9ff
boni37 <73605283+boni37@users.noreply.github.com>
2021-01-12 18:37:14 +0100

    activity

--- src/main/java/activity/TrackPoint.java
+++ src/main/java/activity/TrackPoint.java
@@ -21,9 +21,13 @@ public class TrackPoint {
 
     public double getDistanceFrom(TrackPoint point){
         TrackPoint trackPoint = new TrackPoint(new Coordinate(12.5, 34.89), 123);
-        double distance = Math.sqrt(Math.pow(point.coordinate.getLatitude()-trackPoint.coordinate.getLatitude(),2)
-                +Math.pow(point.coordinate.getLongitude()-trackPoint.coordinate.getLongitude(),2)
-                +Math.pow(point.getElevation()-trackPoint.getElevation(),2));
-        return distance;
+        double R = 6378.137;
+        double dLat = point.coordinate.getLatitude()*Math.PI/180-trackPoint.coordinate.getLatitude()*Math.PI/180;
+        double dLon = point.coordinate.getLongitude()*Math.PI/180-trackPoint.coordinate.getLongitude()*Math.PI/180;
+        double a = Math.sin(dLat/2)*Math.sin(dLat/2) + Math.cos(trackPoint.coordinate.getLatitude()*Math.PI/180)*Math.cos(point.coordinate.getLatitude()*Math.PI/180)* Math.sin(dLon/2)*Math.sin(dLon/2);
+        double c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double d = R*c;
+        System.out.println(d);
+        return d*1000;
     }
 }
