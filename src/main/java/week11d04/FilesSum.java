19b3e1eaf4101d206baab538026902362732eba7
boni37 <73605283+boni37@users.noreply.github.com>
2021-01-14 16:15:40 +0100

    week11d04

---
+++ src/main/java/week11d04/FilesSum.java
@@ -0,0 +1,30 @@
+package week11d04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilesSum {
+
+    public static void main(String[] args) {
+        int numSum = 0;
+        for(int i = 0; i< 100; i++) {
+            String fileName = "number"+i +".txt";
+            Path file = Path.of(fileName);
+            boolean is_regular = Files.isRegularFile(file, LinkOption.NOFOLLOW_LINKS);
+            if(is_regular){
+                try {
+                    String number = Files.readString(file);
+                    numSum += Integer.parseInt(number);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }continue;
+        }
+        System.out.println(numSum);
+    }
+}
