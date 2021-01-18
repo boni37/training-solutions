a9b453a3374000078acca3a8485c6359fde2a267
boni37 <73605283+boni37@users.noreply.github.com>
2021-01-18 15:22:08 +0100

    week12d01

--- src/main/java/week12/d01/GradeRounder.java
+++ src/main/java/week12/d01/GradeRounder.java
@@ -7,7 +7,7 @@ public class GradeRounder {
         for(int i =0; i< grades.length; i++){
             if(grades[i]<40){
                 gradesAfterRound[i] = grades[i];
-            }else if((grades[i]%5)<3){
+            }else if((((grades[i]/5+1)*5-grades[i])>0)&&((((grades[i]/5+1)*5-grades[i])<3))){
                 gradesAfterRound[i] = (grades[i]/5+1)*5;
             }else gradesAfterRound[i] = grades[i];
         }return gradesAfterRound;
