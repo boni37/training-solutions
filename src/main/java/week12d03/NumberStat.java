2c8d6a3fca4e8cb013e1b294e55ceca325b8b79b
boni37 <73605283+boni37@users.noreply.github.com>
2021-01-20 15:25:06 +0100

    week12d03

---
+++ src/main/java/week12d03/NumberStat.java
@@ -0,0 +1,29 @@
+package week12d03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class NumberStat {
+
+
+    public int countNumAppearOnce(List<Integer> numbers){
+        if(numbers.size() < 1) throw new IllegalArgumentException("The list is too short");
+        List<Integer> results = new ArrayList<>();
+        for(Integer actual: numbers){
+            if((numbers.indexOf(actual)== (numbers.lastIndexOf(actual)))){
+                results.add(actual);
+            }
+        }
+        int min=0;
+        for(int i = 1; i< results.size();i++){
+            if(results.get(i)< results.get(min)) min = i;
+        }return results.get(0);
+    }
+
+    public static void main(String[] args) {
+        NumberStat numberStat =new NumberStat();
+        System.out.println(numberStat.countNumAppearOnce(new ArrayList<>(Arrays.asList(1,1,5,3,4,5,6,5,6,4,1,6,5,4))));
+    }
+}
