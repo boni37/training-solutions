5dec33e765c755a8d2db617ab49a97749543f85a
boni37 <73605283+boni37@users.noreply.github.com>
2021-01-31 12:00:01 +0100

    week13d05

--- src/main/java/week13d05/LettersCounter.java
+++ src/main/java/week13d05/LettersCounter.java
@@ -5,12 +5,13 @@ public class LettersCounter {
     public int differentLetters(String word){
         if(word.isBlank() || word.isEmpty()) throw new IllegalArgumentException("Word is empty.");
         int summ =0;
+        String s = word.toLowerCase();
         for(int i=0; i< word.length();i++){
-            if(word.indexOf(word.charAt(i))==word.lastIndexOf(word.charAt(i))) summ++;
+            if(s.indexOf(s.charAt(i))==s.lastIndexOf(s.charAt(i))) summ++;
         }return summ;
     }
 
     public static void main(String[] args) {
-        System.out.println(new LettersCounter().differentLetters("TornadofighterJet"));
+        System.out.println(new LettersCounter().differentLetters("TornaTdofighTjterJet"));
     }
 }
