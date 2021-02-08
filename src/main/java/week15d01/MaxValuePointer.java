e8184780cf78a15bae875a4b82c83a3485e40896
boni37 <73605283+boni37@users.noreply.github.com>
2021-02-08 17:08:11 +0100

    week15d01

---
+++ src/main/java/week15d01/MaxValuePointer.java
@@ -0,0 +1,22 @@
+package week15d01;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MaxValuePointer {
+
+    public static void main(String[] args) {
+        Map<Integer, Double> functionMax = new TreeMap<>();
+        for (int x = -20; x < 21; x++) {
+            Double yValue = (double) x * x + 2 * x + 3;
+            functionMax.put(x, yValue);
+        }
+        Double maxY = Collections.max(functionMax.values());
+        for(Map.Entry entry:functionMax.entrySet()){
+            if(entry.getValue()==maxY){
+                System.out.println((Integer) entry.getKey());
+            }
+        }
+    }
+}
