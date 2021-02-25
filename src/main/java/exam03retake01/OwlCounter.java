71b89ca1d3febb01ffc451f552b0168ea0e032df
boni37 <73605283+boni37@users.noreply.github.com>
2021-02-25 15:54:43 +0100

    exam03retake

--- src/main/java/exam03retake01/OwlCounter.java
+++ src/main/java/exam03retake01/OwlCounter.java
@@ -1,4 +1,25 @@
 package exam03retake01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class OwlCounter {
+
+    private Map<String, Integer> owlsByCounty = new HashMap<>();
+
+    public void readFromFile(BufferedReader reader)throws IOException {
+        String line;
+        while((line=reader.readLine())!=null){
+            String[] counties = line.split("=");
+            String countyName = counties[0];
+            int numberOfOwls = Integer.parseInt(counties[1]);
+            owlsByCounty.put(countyName,numberOfOwls);
+        }
+    }
+
+    public int getNumberOfOwls(String county){
+        return owlsByCounty.get(county);
+    }
 }
