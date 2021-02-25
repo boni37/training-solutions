b504b8c76b3477a4c84fb5b162885f9abaf2886a
boni37 <73605283+boni37@users.noreply.github.com>
2021-02-25 17:25:59 +0100

    exam03retake

--- src/main/java/exam03retake01/MailBox.java
+++ src/main/java/exam03retake01/MailBox.java
@@ -1,4 +1,47 @@
 package exam03retake01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailBox {
+
+    private List<Mail> mails = new ArrayList<>();
+
+    public MailBox(List<Mail> mails) {
+        this.mails = mails;
+    }
+
+    public MailBox() {
+    }
+
+    public List<Mail> getMails() {
+        return mails;
+    }
+
+    public void addMail(Mail mail){
+        mails.add(mail);
+    }
+
+    public List<Mail> findByCriteria(String criteria){
+        List<Mail> resultList = new ArrayList<>();
+        for(Mail actual : mails){
+            if(criteria.startsWith("from:")){
+                String from = criteria.substring(5);
+                if(actual.getFrom().getEmail().equals(from)){
+                    resultList.add(actual);
+                }
+            }
+            else if(criteria.startsWith("to:")){
+                String to = criteria.substring(3);
+                for(Contact c:actual.getTo()){
+                    if(c.getName().equals(to)){
+                        resultList.add(actual);
+                    }
+                }
+            }
+            else if(actual.getSubject().contains(criteria) | actual.getMessage().contains(criteria)){
+                    resultList.add(actual);
+            }
+        }return resultList;
+    }
 }
